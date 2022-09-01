adae75a6af46508d19f0224df00aa79f280dba79
FeMCDias <femcdias@gmail.com>
2022-09-01 15:05:57 -0300

    Ex 1 Completo Atrasado

--- matematica/Calculadora.py
+++ matematica/Calculadora.py
@@ -18,9 +18,14 @@ def multiplicacao(va: float, vb: float):
 def divisao(va: float, vb: float):
     ''' Função que retorna a divisão entre dois valores
     '''
+    if vb == 0:
+        return np.inf
     return va / vb
 
 def media_lista_valores(v:list):
     ''' Função que retorna a média entre N valores
     '''
+    if len(v) == 0:
+        return 0
+    v = [num for num in v if isinstance(num, (int, float))]
     return np.mean(v)
---
+++ test/test_operacoes.py
@@ -0,0 +1,21 @@
+import pytest
+from matematica.Calculadora import *
+
+
+@pytest.mark.op_simples
+def test_soma_dois_valores_positivos():
+    v1 = 5.0
+    v2 = 7.0
+    assert 12 == soma(v1, v2)
+@pytest.mark.exercicio_1
+def test_divisao_por_zero():
+    v1 = 5.0
+    v2 = 0.0
+    assert np.inf == divisao(v1, v2)
+@pytest.mark.exercicio_1
+def test_media_lista_valores():
+    v1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    assert 5.5 == media_lista_valores(v1)
+    v2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'a']
+    assert 5.5 == media_lista_valores(v2)
+    assert 0 == media_lista_valores([])
